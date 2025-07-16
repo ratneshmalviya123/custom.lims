d395a6fa5a483b0adfaafbb156febe7053b9493a
Ratnesh Malviya <ratneshmalviya123@gmail.com>
2025-07-16 18:29:29 -0400

    changes for working PlateId & Well Position

--- src/custom/lims/adapters/listing.py
+++ src/custom/lims/adapters/listing.py
@@ -15,10 +15,6 @@ from zope.interface import implements
 
 # Columns to add
 ADD_COLUMNS = [
-    # ("BatchSize", {
-    #     "title": _("BatchSize"),
-    #     "sortable": False,
-    # }),
     ("WellPosition ", {
         "title": _("Well Position"),
         "sortable": True,
@@ -26,55 +22,122 @@ ADD_COLUMNS = [
 ]
 
 
-class SamplesListingAdapter(object):
-    """Generic adapter for samples listing views
-    """
+class SamplesListingViewAdapter(object):
     adapts(IListingView)
     implements(IListingViewAdapter)
-
-    # Priority order of this adapter over others
-    priority_order = 99999
 
     def __init__(self, listing, context):
         self.listing = listing
         self.context = context
 
-    @property
-    @memoize
-    def senaite_theme(self):
-        return getMultiAdapter(
-            (self.context, self.listing.request),
-            name="senaite_theme")
-
-    def icon_tag(self, name, **kwargs):
-        return self.senaite_theme.icon_tag(name, **kwargs)
-
-    @check_installed(None)
-    def folder_item(self, obj, item, index):
-
-        # batch_batch_size = api.get_object(obj).getBatchSize()
-        # batch_ReleasedQuantity = api.get_object(obj).getReleasedQuantity()
-        # item["BatchSize"] = batch_batch_size
-        # item["ReleasedQuantity"] = batch_ReleasedQuantity
-        
-        well_position = api.get_object(obj).getWellPosition()
-        item["WellPosition"] = well_position
-
-        return item
-
-
-    @check_installed(None)
     def before_render(self):
-        # Additional columns
-        rv_keys = map(lambda r: r["id"], self.listing.review_states)
-        for column_id, column_values in ADD_COLUMNS:
-            add_column(
-                listing=self.listing,
-                column_id=column_id,
-                column_values=column_values,
-                after=column_values.get("after", None),
-                review_states=rv_keys)
+        # Add new column for all available states
+        states = map(lambda r: r["id"], self.listing.review_states)
+        add_column(
+            listing=self.listing,
+            column_id="WellPosition",
+            column_values={
+                "title": "My new column",
+                "sortable": False,
+            },
+            review_states=states)
+
+    def folder_item(self, obj, item, index):
+        item["WellPosition"] = api.get_object(obj).getWellPosition()
+        return item
+    
+
+
+
+
+# @property
+# @memoize
+# def senaite_theme(self):
+#     return getMultiAdapter(
+#         (self.context, self.listing.request),
+#         name="senaite_theme")
+
+# def icon_tag(self, name, **kwargs):
+#     return self.senaite_theme.icon_tag(name, **kwargs)
+
+# def folder_item(self, obj, item, index):
+
+# batch_batch_size = api.get_object(obj).getBatchSize()
+# batch_ReleasedQuantity = api.get_object(obj).getReleasedQuantity()
+# item["BatchSize"] = batch_batch_size
+# item["ReleasedQuantity"] = batch_ReleasedQuantity
+
+# well_position = api.get_object(obj).getWellPosition()
+# item["WellPosition"] = well_position
+
+# sample = api.get_object(obj)
+# item["WellPosition"] = obj.getField("wellPosition").get(sample) or "Empty value"
+
+# return item
+
+
+
+# def before_render(self):
+#     # Additional columns
+#     rv_keys = map(lambda r: r["id"], self.listing.review_states)
+#     for column_id, column_values in ADD_COLUMNS:
+#         add_column(
+#             listing=self.listing,
+#             column_id=column_id,
+#             column_values=column_values,
+#             after=column_values.get("after", None),
+#             review_states=rv_keys)
+        
+#     review_states = [self.listing.review_states[0]]
+#     for review_state in review_states:
+#         review_state.update({"columns": self.listing.columns.keys()})
+        
+# def before_render(self):
+# Add a new filter status
+# draft_status = {
+#     "id": "draft",
+#     "title": "Draft",
+#     "contentFilter": {
+#         "review_state": "sample_draft",
+#         "sort_on": "created",
+#         "sort_order": "descending",
+#     },
+#     "columns": self.listing.columns.keys(),
+# }
+# self.listing.review_states.append(draft_status)
+
+# Add the column
+# self.listing.columns["WellPosition"] = {
+#     "title": "Well Position",
+#     "sortable": False,
+#     "toggle": True,
+# }
+
+# Make the new column visible for all filter statuses
+# for filter in self.listing.review_states:
+#     filter.update({"columns": self.listing.columns.keys()})
             
-        review_states = [self.listing.review_states[0]]
-        for review_state in review_states:
-            review_state.update({"columns": self.listing.columns.keys()})
+            
+
+# @implementer(IListingViewAdapter)
+# class CustomSampleListingAdapter(object):
+#     def __init__(self, view, context):
+#         self.view = view
+#         self.context = context
+
+#     def folder_columns(self):
+#         columns = self.view.folder_columns()
+#         columns["Well Position"] = {
+#             "title": "Well Position",
+#             "index": "getWellPosition",
+#             "toggle": True,
+#         }
+#         return columns
+
+#     def folderitems(self, items):
+#         for item in items:
+#             obj = item.get("obj")
+#             if not obj:
+#                 continue
+#             item["Well Position"] = getattr(obj, "getWellPosition", lambda: "")()
+#         return items
--- src/custom/lims/extenders.py
+++ src/custom/lims/extenders.py
@@ -5,7 +5,6 @@ from plone.app.content.interfaces import INameFromTitle
 # from bika.lims.interfaces import ISample
 # from bika.lims.interfaces import ISample
 # from senaite.lims.content.sample import Sample
-from archetypes.schemaextender.interfaces import ISchemaExtender, IBrowserLayerAwareExtender
 from archetypes.schemaextender.interfaces import ISchemaModifier
 from Products.Archetypes import atapi
 from Products.Archetypes.public import AnnotationStorage, AttributeStorage, StringField, StringWidget
@@ -14,13 +13,114 @@ from custom.lims.interfaces import ICustomLimsLayer  # your browser layer
 
 from plone.supermodel import model
 from zope import schema
-# from bika.lims.content.analysisrequest import IAnalysisRequest
 from bika.lims.interfaces import IAnalysisRequest
 from zope.interface import implementer
 from zope.schema.fieldproperty import FieldProperty
+from bika.lims.content.analysisrequest import AnalysisRequest    
+from zope.interface import implements, implementer
+from zope.component import adapts
+from archetypes.schemaextender.interfaces import ISchemaExtender, IBrowserLayerAwareExtender, IOrderableSchemaExtender
+from archetypes.schemaextender.field import ExtensionField
+from Products.Archetypes.Field import StringField, IntegerField, ReferenceField
+from Products.Archetypes.Widget import IntegerWidget, StringWidget, ReferenceWidget
 
-from bika.lims.content.analysisrequest import AnalysisRequest
+class StringExtensionField(ExtensionField, StringField):
+    pass
 
+class IntegerExtensionField(ExtensionField, IntegerField):
+    pass
+
+class ReferenceExtensionField(ExtensionField, ReferenceField):
+    pass
+
+
+WellPosition = StringExtensionField(
+                name="wellPosition",
+                # label=u"Well Position", 
+                widget=StringWidget(
+                    # description=u"Enter Well Position",
+                    label=u"Well Position", 
+                    visible={'view': 'visible', 
+                             'edit': 'visible', 
+                             'header_table': 'visible', 
+                             'add' : 'edit'},
+                    custom_js=(
+                        u"jq(document).ready(function() {"
+                        u"  setTimeout(function() {"
+                        u"    jq('input[name$=wellPosition-0]').attr('placeholder', 'Enter Well Position');"
+                        u"  }, 100);"
+                        u"});",
+                    ),
+                ),
+                required=False,
+            )
+
+PlateID = StringExtensionField(
+                name="plateID",
+                # label=u"Plate ID ",
+                widget=StringWidget(
+                    # label=u"\u200B",  # zero-width space (prevents label fallback) 
+                    label=u"Plate ID ",
+                    description=u"If Plate is submitted, enter Plate ID",
+                    visible={'view': 'visible', 
+                             'edit': 'visible', 
+                             'header_table': 'visible', 
+                             'add' : 'edit'},
+                    custom_js=(
+                        u"jq(document).ready(function() {"
+                        u"  setTimeout(function() {"
+                        u"    jq('input[name$=plateID-0]').attr('placeholder', 'If Plate is submitted, enter Plate ID');"
+                        u"  }, 100);"
+                        u"});",
+                    ),
+
+                ),
+                required=False,
+            )
+
+PlateID = StringExtensionField(
+    name="plateID",
+    widget=StringWidget(
+        label=u"Plate ID",
+        description=u"",  # leave this blank if using placeholder
+        visible={
+            'view': 'visible',
+            'edit': 'visible',
+            'header_table': 'visible',
+            'add': 'edit',
+        }
+    ),
+    required=False,
+)
+@implementer(IOrderableSchemaExtender)
+class ExtraARFields(object):
+    """Schema extender for the Sample content type."""
+    adapts(IAnalysisRequest)
+    # implements(ISchemaExtender)
+    # layer = ICustomLimsLayer
+
+    def __init__(self, context):
+        self.context = context
+
+    def getOrder(self, schematas):
+        return schematas
+    
+    def getFields(self):
+        return [
+            WellPosition, 
+            PlateID
+        ]
+        
+    # def getWellPosition(self):
+    #     import logging; logging.getLogger().info("Fetching wellPosition")
+    #     return self.getField("wellPosition").get(self)
+
+    # def setWellPosition(self, value):
+    #     return self.getField("wellPosition").set(self, value)
+    
+    # AnalysisRequest.getWellPosition = getWellPosition
+    # AnalysisRequest.setWellPosition = setWellPosition
+    
 # class ICustomAnalysisRequest(IAnalysisRequest):
 #     """Custom schema for Analysis Request"""
 
@@ -47,52 +147,3 @@ from bika.lims.content.analysisrequest import AnalysisRequest
 #     def getFields(self):
 #         return self.fields
     
-class ExtraARFields(object):
-    implements(ISchemaExtender)
-    adapts(IAnalysisRequest)
-    # adapts(ISample)
-
-    fields = [
-        StringField(
-            "wellPosition",
-            # storage=AnnotationStorage(),
-            storage=AttributeStorage(),
-            required=False,
-            # mutator="setWellPosition",
-            # accessor="getWellPosition",
-            widget=StringWidget(
-                # label="Well Position",
-                visible={'view': 'visible', 'edit': 'visible', 'header_table': 'visible', 'add' : 'edit'},
-            ),
-        )
-    ]
-
-    def __init__(self, context):
-        self.context = context
-
-    def getFields(self):
-        return self.fields
-
-    # Define accessor/mutator methods
-    # def getWellposition(self):
-    #     return self.context.getField("wellPosition").get(self.context)
-
-    # def setWellposition(self, value):
-    #     return self.context.getField("wellPosition").set(self.context, value)
-    
-    # def getWellPosition(self):
-    #     import logging; logging.getLogger().info("Fetching wellPosition")
-    #     return self.getField("wellPosition").get(self)
-
-
-    # def setWellPosition(self, value):
-    #     return self.getField("wellPosition").set(self, value)
-    
-    # def setWellPosition(self, value):
-    #     import logging
-    #     logging.getLogger().info("Setting wellPosition = %s", value)
-    #     return self.getField("wellPosition").set(self, value)
-
-    # AnalysisRequest.getWellPosition = getWellPosition
-    # AnalysisRequest.setWellPosition = setWellPosition
-
